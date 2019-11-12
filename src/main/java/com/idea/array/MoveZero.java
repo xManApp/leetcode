1736302328a9121ed3bd0a2818b572a138c01f4f
Kimmy <952451021@qq.com>
2019-11-12 10:22:15 +0800

    加入注释

--- src/main/java/com/idea/array/MoveZero.java
+++ src/main/java/com/idea/array/MoveZero.java
@@ -3,32 +3,26 @@ package com.idea.array;
 /**
  * 283 移动零
  * @author 赵继
- *         2019/11/12.
+ * 2019/11/12.
  */
 public class MoveZero {
 
-    public static void main(String[] args) {
-        int[] nums = {1, 0, 1};
-        int j = 0; // 存放下一个非0整数的位置
-        for (int i=0; i < nums.length; i++) {
-
+    // j放置非0的数
+    // i和j同时向前移动 遇到为0的数，j停下
+    // i继续向前，nums[i]不为0，为就交换位置
+    // 因为j已经在可以放置非0的位置上等上了  也就是在为0的位置上等上了
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for (int i=0; i<nums.length;i++) {
             if (nums[i]!=0) {
-
-                if (i!=j) {
+                if (i!=j){
+                    int tmp = nums[j];
                     nums[j] = nums[i];
-                    nums[i] = 0;
+                    nums[i] = tmp;
                 }
                 j++;
             }
-
         }
-
-        for (int i=0; i < nums.length; i++) {
-
-            System.out.println(nums[i]);
-
-        }
-
     }
 
 
